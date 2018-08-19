4b511e4e9f312f1c8b583737f1c2b4f1da0508a6
Nico Verbruggen <nico.verb@gmail.com>
2018-08-19 19:24:00 +0200

    Added a few examples with imports

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
-var hello = "test";
+// Since we're using Laravel Mix and its Webpack configuration,
+// we can import functionality that was exported in other files.
+import { Person } from './samples/person';
+import { add } from './samples/addition';
+
+// Try to use our imported function.
+console.log(add(2, 5));
+
+// Let's create a new person.
+let person = new Person("John Appleseed");
+person.setBirthYear(2000);
+console.log(person);
+
+// If we get here, our app works. Great!
+console.log("Looks like our app is working.");
---
+++ src/js/samples/person.js
@@ -0,0 +1,11 @@
+export class Person {
+    constructor(name) {
+        this.name = name;
+        this.birth_year = null;
+    }
+
+    setBirthYear(year) {
+        this.birth_year = year;
+    }
+}
+
